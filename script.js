e3e06a6542ccb9a524b95c5fcfb38902c2cad5a7
kiransaju11 <kiransaju39@gmail.com>
2026-02-20 15:29:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ let count = 0;
 function increaseCount() {
     count++;
     document.getElementById("counter").innerText = count;
+
+    document.body.style.backgroundColor =
+        "hsl(" + (count * 30 % 360) + ", 70%, 20%)";
 }
